10d83956e67395eb00d7d75f8675403b8458318c
Mak Yen Wei <shaonmak@gmail.com>
2021-06-09 21:59:05 +0800

    feat: update user route module

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -55,48 +55,50 @@ const updateAllAccessToken = async(req, res) => {
 
     let updateAllUsers = new Promise((resolve, reject) => {
         userList.forEach( async(user, index, array) => {
-            var data = JSON.stringify(
-                {
-                    "grant_type": "refresh_token",
-                    "client_id": "602ce67ff5b1b0e7483b1132",
-                    "client_secret": "7e9a5612-1c06-4dd8-9aa1-3544d2a37d09",
-                    "refresh_token": `${user.dataValues.refresh_token}`
-                }
-            );
-    
-            var config = {
-                method: 'post',
-                url: 'https://raindrop.io/oauth/access_token',
-                headers: { 
-                    'Content-Type': 'application/json', 
-                },
-                data : data
-            };
-    
-            await axios(config)
-                .then((res) => {
-                    User.update(
-                        {
-                            access_token: res.data.access_token,
-                            refresh_token: res.data.refresh_token
-                        },
-                        {
-                            where:{
-                                user_id: user.dataValues.user_id
+            if(user.dataValues.refresh_token){
+                var data = JSON.stringify(
+                    {
+                        "grant_type": "refresh_token",
+                        "client_id": "602ce67ff5b1b0e7483b1132",
+                        "client_secret": "7e9a5612-1c06-4dd8-9aa1-3544d2a37d09",
+                        "refresh_token": `${user.dataValues.refresh_token}`
+                    }
+                );
+        
+                var config = {
+                    method: 'post',
+                    url: 'https://raindrop.io/oauth/access_token',
+                    headers: { 
+                        'Content-Type': 'application/json', 
+                    },
+                    data : data
+                };
+        
+                await axios(config)
+                    .then((res) => {
+                        User.update(
+                            {
+                                access_token: res.data.access_token,
+                                refresh_token: res.data.refresh_token
+                            },
+                            {
+                                where:{
+                                    user_id: user.dataValues.user_id
+                                }
                             }
-                        }
-                    ).then((res) => {
-                        updatedUsers++;
+                        ).then((res) => {
+                            updatedUsers++;
+                        })
+                        .catch((err) => {
+                            console.log(err)
+                        })
                     })
                     .catch((err) => {
                         console.log(err)
                     })
-                })
-                .catch((err) => {
-                    console.log(err)
-                })
+            }
 
-                if (index === array.length -1) resolve();
+            if (index === array.length -1) resolve();
         });
     });
 
